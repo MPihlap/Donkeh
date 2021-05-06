81d17a6657912461d6f3f349d1b1a3271d47f5f0
Meelis Pihlap <pihlap@ut.ee>
2021-05-06 19:57:07 +0300

    minor adjustments

--- get_train_put.ps1
+++ get_train_put.ps1
@@ -1,6 +1,6 @@
 param (
     $ip="donkeh1.local",
-    $tub_id,
+    $id,
     [switch]$force_all,
     [switch]$force_copy,
     [switch]$force_convert,
@@ -9,15 +9,15 @@ param (
 
 $ErrorActionPreference = "Stop" # Stop script if a single line fails
 
-if (!(Test-Path "data/tub_$tub_id*") -or $force_all -or $force_copy) {
-    scp -r ("pi@$ip"+":/home/pi/mycar/data/tub_$tub_id*") "data/"
+if (!(Test-Path "data/tub_$id*") -or $force_all -or $force_copy) {
+    scp -r ("pi@$ip"+":/home/pi/mycar/data/tub_$id*") "data/"
 }
 else {
-    Write-Host "Tub with id $tub_id already exists, skipping copy"
-    Write-Host "Run with -force_all or -force_copy to override"
+    Write-Host "Tub with id $id already exists, skipping copy"
+    Write-Host "Run with -force_all or -force_copy to override `n"
 }
 
-$tub_path = $(Get-ChildItem "data/tub_$tub_id*")[0]
+$tub_path = $(Get-ChildItem "data/tub_$id*")[0]
 $new_tub_path = $tub_path.FullName + "_v2"
 
 if (!(Test-Path $new_tub_path) -or $force_all -or $force_convert) {
@@ -25,17 +25,17 @@ if (!(Test-Path $new_tub_path) -or $force_all -or $force_convert) {
 }
 else {
     Write-Host "Tub $new_tub_path already exists, skipping convert"
-    Write-Host "Run with -force_all or -force_convert to override"
+    Write-Host "Run with -force_all or -force_convert to override `n"
 }
 
-$model_path = "models/model_$tub_id.h5"
+$model_path = "models/model_$id.h5"
 
 if (!(Test-Path $model_path) -or $force_all -or $force_train) {
     donkey train --tub $new_tub_path --model $model_path
 }
 else {
     Write-Host "Model $model_path already exists, skipping training"
-    Write-Host "Run with -force_all or -force_train to override"
+    Write-Host "Run with -force_all or -force_train to override `n"
 }
 
 scp $model_path ("pi@$ip"+":/home/pi/mycar/models/")
